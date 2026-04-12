f2f878c2a21b4abf8cb87d0a382c9a09a53ad6ec
Sagnik Dasgupta <sagnik2000trainstation@gmail.com>
2026-04-12 20:27:33 +0530

    Latest

--- src/main/java/net/sagnik/newmod/NewMod.java
+++ src/main/java/net/sagnik/newmod/NewMod.java
@@ -21,7 +21,7 @@ public class NewMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "newmod";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public NewMod(FMLJavaModLoadingContext context)
     {
